af43916b09d7b99d8f57386f54e90bdaf51893ac
timkofu <509078+timkofu@users.noreply.github.com>
2019-12-02 21:56:20 +0400

    coursework_two

--- classy/controller/tasks/5/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-[Deeper analysis of the data: the data is split into 10 classes, search for important attributes for
-each class] For each train_smpl_<label> file:
-● Using the Weka facility “Select Attributes” for each of these 10 files, record the first 10 fields,
-in order of the absolute correlation value, for each street sign.
-
-"""
--- classy/controller/tasks/four/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-[Classification: Performance of the Naive Bayes algorithm on the given data set] Run the Naive
-Bayes tool in Weka on the resulting version of train_gr_smpl. To be able to do this, you may need to
-apply several Weka “Filters”. Explain the reason for choosing and using these filters. Once you can run
-the algorithm, record, compare and analyse the classifier’s accuracy on different classes (as given by the
-Weka Summary and the confusion matrix).
-
-"""
